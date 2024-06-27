0d5f851b02e97bfec4ef6571fb043f44d42be106
Liliya Akhmet <akhmetzianova.lil@yandex.ru>
2024-06-27 21:57:54 +0300

    пагинация

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Card from '../components/Card';
 // import {card} from '../helpers/Array';
 
-
 import backgroundImage from '../img/BackB.svg';
 import Sceleton from '../components/Sceleton';
-import NotFound from './notfound/NotFound';
+// import NotFound from './notfound/NotFound';
 import Nav from '../components/navigation/Nav';
 import Sort from '../components/sort/Sort';
 import Portion from '../components/portion/Portion';
+import Pagination from '../components/pagination/Pagination';
 
 
 
 const Home = ({searchValue}) => {
   const[users, setUsers] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+
+  const[currentPage, setCurrentPage] = React.useState(1)
+
 
    const[categoryNav, setCategoryNav] = React.useState(0);
   const [sortType, setSortType] = React.useState({
@@ -31,7 +34,7 @@ const Home = ({searchValue}) => {
     const sortBy = sortType.sort.replace('-','');
     const search = searchValue ? `&search=${searchValue}`: '';
     
-    fetch(`https://665a0cf9de346625136ee5a1.mockapi.io/users?${categoryNav > 0 ? `category=${categoryNav}`: ''
+    fetch(`https://665a0cf9de346625136ee5a1.mockapi.io/users?page=${currentPage}&limit=3&${categoryNav > 0 ? `category=${categoryNav}`: ''
     }&sortBy=${sortBy}&order=${order}${search}`, 
   ) 
   .then((res) => res.json())
@@ -40,7 +43,7 @@ const Home = ({searchValue}) => {
     setIsLoading(false);
   });
   window.scrollTo(0, 0);
-}, [categoryNav, sortType, searchValue]);
+}, [categoryNav, sortType, searchValue, currentPage]);
 // console.log(categoryNav, sortType)
 
   const usersArr = users.map((obj)=> <Card
@@ -73,6 +76,8 @@ const Home = ({searchValue}) => {
 
       }
       </div> 
+      <Pagination onChangePage={number => setCurrentPage(number)}/>
+      
     </>
       
   );
